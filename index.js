27f129400e33b596dd66023e0b5505a1cad18149
endlaz <lazlovdende@hotmail.com>
2019-10-04 21:16:32 +0200

    Setup socket.io and postfeed route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Server
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`App server up and running on port ${port}`));
+const { app, io } = require('./server');
+
+io.on("connection", socket => {
+    console.log("New client connected");
+    socket.on('testClient', (data) => {
+        console.log('Data from client', data);
+    })
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
 
 // Middleware
 const cors = require('cors');
@@ -14,21 +21,33 @@ app.use(corsMiddleware, bodyParser);
 const db = require('./db');
 const bcrypt = require('bcrypt');
 const User = require('./user/userModel');
-db.sync({force: true})
+const Post = require('./post/postModel');
+db.sync({ force: true })
     .then(() => {
         console.log('Database connected');
-        // Add/create a default user for testing
+        // Add/create a default user and posts for testing
         User.create({
             email: "test@test.com",
             password: bcrypt.hashSync("123456", 10),
             username: "AwesomeUsername",
             name: "Test user",
             bio: "Welcome to my InstaClone profile"
-        })
+        });
+        Post.create({
+            picture: "https://thenypost.files.wordpress.com/2019/09/instagram-private-stories-01.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+            description: "Some random image from the web",
+            userId: 1
+        });
+        Post.create({
+            picture: "https://thenypost.files.wordpress.com/2019/09/instagram-private-stories-01.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+            description: "Another random but the same image from the web",
+            userId: 1
+        });
     })
     .catch(console.error);
 
 // Routes
 const authRouter = require('./auth/router');
 const userRouter = require('./user/userRouter');
-app.use(authRouter, userRouter);
+const postRouter = require('./post/postRouter');
+app.use(authRouter, userRouter, postRouter);
