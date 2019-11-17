2f574ab4e37be80d6b5cce4e73a75c16250315dd
endlaz <lazlovdende@hotmail.com>
2019-11-17 17:23:23 +0100

    Add specific post get endpoint

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -37,14 +37,14 @@ router.post('/post', auth, (req, res, next) => {
 	}
 })
 
- // Add comment to a post
- router.post('/post/:id/comment', auth, (req, res, next) => {
-	const comment = req.body.comment; 
+// Add comment to a post
+router.post('/post/:id/comment', auth, (req, res, next) => {
+	const comment = req.body.comment;
 	const postId = req.params.id;
 	const userId = req.user.id;
 
 	if (comment) {
-		Comment.create({comment, postId, userId})
+		Comment.create({ comment, postId, userId })
 			.then(() => {
 				res.status(201).send({ message: "Comment added" });
 			})
@@ -52,6 +52,35 @@ router.post('/post', auth, (req, res, next) => {
 	} else {
 		res.status(400).send({ message: "Can't add an empty comment" });
 	}
- })
+})
+
+// Get one post
+router.get('/post/:id', auth, (req, res, next) => {
+	Post.findByPk(req.params.id, {
+		include: [
+			{
+				model: User,
+				attributes: ['username']
+			},
+			{
+				model: Comment,
+				include: [
+					{
+						model: User,
+						attributes: ['username']
+					}
+				]
+			}
+		]
+	})
+		.then(post => {
+			if (post) {
+				res.status(200).send(post);
+			} else {
+				res.status(404).send({ message: "Post not found" });
+			}
+		})
+		.catch(next);
+})
 
 module.exports = router;
